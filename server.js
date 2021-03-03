352a4b4cfa72c3694f1dd1a4325beb10ce77bdb6
Marshallb98 <69322012+Marshallb98@users.noreply.github.com>
2021-03-02 23:30:18 -0600

    added mongodb connection

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ app.use(express.json());
 app.use(morgan("dev"))
 
 app.use(express.static("public"))
+const databaseUrl = "workouts";
+const collection = ["exercises"];
+const db = mongojs(databaseUrl, collection);
+
+db.on("error", error => {
+    console.log("Database Error:", error);
+});
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
@@ -18,6 +25,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useCreateIndex: true,
     useFindAndModify: false 
 })
+
+app.use(require("./routes/html-routes.js"));
+
+app.use(require("./routes/api-routes.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on ${PORT}!`)
